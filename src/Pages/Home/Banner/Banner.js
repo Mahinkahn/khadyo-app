0050d50207fea118a6f7d3573975a7bd44a7fd3a
Mahinkahn <mahinkahn37@gmail.com>
2023-03-15 21:22:41 +0600

    newsd

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -11,8 +11,9 @@ import Gb from '../../../assets/images/Gb.png';
 import Round from '../../../assets/images/Round.png';
 import pata from '../../../assets/images/pata.png';
 import g from '../../../assets/images/G.png';
-import Vector2 from '../../../assets/images/Vector.png';
+import V from '../../../assets/images/V.png';
 import Vector5 from '../../../assets/images/Vector5.png';
+import Rectangle from '../../../assets/images/Rectangle 4.png';
 import Bannercss from '../Banner/Banner.css';
 
 import { TypeAnimation } from 'react-type-animation';
@@ -41,46 +42,65 @@ const Banner = () => {
 
                 <img src={Mask} alt="" className=' w-[201vh] h-[95vh]' />
 
-                <div className=' flex ml-[-85%] gap-32 mt-[-15%]'>
+                <div className=' flex ml-[-83%] gap-44 mt-[-10%]'>
                     <img src={Gro2} alt="" />
                     <img src={Gro2} alt="" />
                 </div>
 
-                <div className='mt-[-20%] ml-[-72%]'>
-                    <img src={text} alt="" className='w-full ' />
-                </div>
 
-                <img src={Vector2} alt="" className='w-[80px] ml-[-40%]' />
+                <img src={text} alt="" className=' w-96 h-[260px] mt-[-16%] ml-[-68%]' />
+
+
+                <img src={V} alt="" className=' ml-[-71%] mt-44' />
+                <img src={Rectangle} alt="" className='Rectangle ml-[-71%] mt-48' />
 
                 <div className="hero-content text-center ml-[-10%] mt-[-50px]  text-neutral-content">
 
                     <h1 className=" text-6xl font-bold bg-secondary rounded-2xl w-[210px] h-[83px] text-[#582A18] font-serif py-3">Fresh
                     </h1>
-                    <h1 className=" text-5xl font-bold">
-                        <TypeAnimation
-                            sequence={[
-                                'Coffee', // Types 'Coffee'
-                                2500, // Waits 3s
-                                'Experience', // Deletes 'One' and types 'Experience'
-                                4000, // Waits 2s
-
-                                () => {
-                                    console.log('Done typing!'); // Place optional callbacks anywhere in the array
-                                }
-                            ]}
-                            wrapper="div"
-                            cursor={true}
-                            repeat={Infinity}
-                            style={{ fontSize: '' }}
-                        />
-                    </h1>
-
-
                 </div>
+                <h1 className=" text-6xl font-bold text-neutral-content ml-64 mt-[-34px] font-serif">
+                    <TypeAnimation
+                        sequence={[
+                            'Coffee', // Types 'Coffee'
+                            2500, // Waits 3s
+                            'Fresh', // Deletes 'One' and types 'Fresh'
+                            4000, // Waits 2s
+
+                            () => {
+                                console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                            }
+                        ]}
+                        wrapper="div"
+                        cursor={true}
+                        repeat={Infinity}
+                        style={{ fontSize: '' }}
+                    />
+                </h1>
+                <h1 className=" text-6xl font-bold text-neutral-content ml-6 mt-[144px] font-serif">
+                    <TypeAnimation
+                        sequence={[
+                            'Experience', // Types 'Coffee'
+                            2500, // Waits 3s
+                            'Coffee', // Deletes 'One' and types 'Fresh'
+                            4000, // Waits 2s
+
+                            () => {
+                                console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                            }
+                        ]}
+                        wrapper="div"
+                        cursor={true}
+                        repeat={Infinity}
+                        style={{ fontSize: '' }}
+                    />
+                </h1>
+
             </div>
-            <div className=' flex justify-center mt-[-14%] '>
-                <img src={CoCup} alt="" className='' />
-            </div>
+
+            <img src={CoCup} alt="" className=' mt-[-18%] ml-[32%] w-[35%] cop' />
+
+
             <div className=' flex justify-between mt-[-180px]'>
                 <img src={Vector} alt="" />
                 <img src={Group} alt="" />
