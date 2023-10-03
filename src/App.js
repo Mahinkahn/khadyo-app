175392bd1536e25da3c21cb95b0187ed27d587ff
Mahinkahn <mahinkahn37@gmail.com>
2023-10-03 10:59:04 +0600

    sdhihsifdus

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "slick-carousel/slick/slick-theme.css";
 import 'aos/dist/aos.css';
 import './App.css';
 import router from './Routes/Routes/Routes';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Loader from './components/loader/Loader';
+
 
 
 function App() {
@@ -14,7 +16,22 @@ function App() {
     Aos.init();
   }, []);
 
-  return (
+  // loader state
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Let create async method to fetch fake data
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 4000);
+    };
+
+    fakeDataFetch();
+  }, []);
+
+  return isLoading ? (
+    <Loader />) : (
     <div>
       <RouterProvider router={router}></RouterProvider>
     </div>
--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -18,42 +18,14 @@ import './Testimonial.css'
 const Testimonial = () => {
 
 
-    var settings = {
-
+    const settings = {
 
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
-        speed: 2000,
         autoplaySpeed: 2000,
-
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+        pauseOnHover: true
     };
 
 
@@ -87,16 +59,16 @@ const Testimonial = () => {
             location: 'With a similar flavor to black coffee, the americano'
         },
         {
-            _id: 3,
-            name: 'Foil Food Packet',
-            img: people3,
+            _id: 1,
+            name: 'Metallic Coffee',
+            img: people1,
             imga: Group2,
             location: 'With a similar flavor to black coffee, the americano'
         },
         {
-            _id: 3,
-            name: 'Foil Food Packet',
-            img: people3,
+            _id: 2,
+            name: 'Americano Coffee',
+            img: people2,
             imga: Group2,
             location: 'With a similar flavor to black coffee, the americano'
         },
